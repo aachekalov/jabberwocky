9d2298e8b88f17559c792eb152cd54f4f0e77df9
AAChekalov@gmail.com <AAChekalov@gmail.com@f5664db2-0e7f-d17f-f7c6-2a055b35f40f>
2011-05-06 17:43:04 +0000

    create_table

--- trunk/create_table.c
+++ trunk/create_table.c
@@ -5,55 +5,80 @@
 #include "create_table.h"
 #include "parsing_tools.h"
 
-struct create *parse(char *create_query) {
-	struct create *result = (struct create *) malloc(sizeof(struct create));
-
-	trim(create_query);
-
-	size_t query_len = strlen(create_query);
-
-	size_t table_len = strcspn(create_query, "(");
+int check_table_name_length(size_t query_len, size_t table_len) {
 	if (table_len == 0) {
 		printf("table name must be not empty\n");
-		return NULL;
+		return -1;
 	} else if (table_len == query_len) {
 		printf("incorrect query: '(' not found\n");
-		return NULL;
+		return -1;
 	} else if (table_len > 18) {
 		printf("table name lenght must be <= 18\n");
+		return -1;
+	}
+	return 0;
+}
+
+char *parse_table_name(char *create_query, size_t query_len, size_t table_len) {
+	if (check_table_name_length(query_len, table_len)) {
 		return NULL;
 	}
 	char *table_name = (char *) malloc(table_len * sizeof(char));
 	strncpy(table_name, create_query, table_len);
 	trim(table_name);
-	if (check_identifier_name(table_name))
+	if (check_identifier_name(table_name)) {
+		free(table_name);
 		return NULL;
+	}
+	return table_name;
+}
 
+char *parse_columns_declare(char *create_query, size_t query_len, size_t table_len) {
+	char *columns_declare = ""; // TODO: malloc
 	int i;
 	for (i = table_len + 1; i < query_len; i++) {
 		if (create_query[i] == ',')
-			strcpy(create_query, table_name);
+			strcpy(columns_declare, create_query);
 	}
+	return columns_declare;
+}
+
+struct create *parse(char *create_query) {
+	struct create *result = (struct create *) malloc(sizeof(struct create));
+	trim(create_query);
+	size_t query_len = strlen(create_query);
+	size_t table_len = strcspn(create_query, "(");
+
+	result->table_name = parse_table_name(create_query, query_len, table_len);
+	if (result->table_name == NULL) {
+		free(result);
+		return NULL;
+	}
+
+	char *columns_declare = parse_columns_declare(create_query, query_len, table_len);
+	if (columns_declare == NULL) {
+		free(result);
+		return NULL;
+	}
+	// result->query = result->table_name + '(' + columns_declare + ')';
+
 	return result;
 }
 
-int create_data_file(char *table_name) {
+int create_table_data_file(char *table_name) {
 	return 0;
 }
 
-int write_to_fd(int fd, char *query) {
+int write_table_structure(int fd, char *query) {
+	printf("DEBUG: table structure:%s\n", query);
 	return 0;
 }
 
-/**
- * @param fd file descriptor main database file
- * @param create_query query such as "table_name (field0 type0 [constraints], ..., fieldN typeN [constraints])"
- */
 int create_table(int fd, char *create_query) {
 	struct create *result = parse(create_query);
 	if (result == NULL)
 		return -1;
-	create_data_file(result->table_name);
-	write_to_fd(fd, result->query);
+	create_table_data_file(result->table_name);
+	write_table_structure(fd, result->query);
 	return 0;
 }
--- trunk/create_table.h
+++ trunk/create_table.h
@@ -3,14 +3,20 @@ struct create {
 	char *query;
 };
 
+int check_table_name_length(size_t query_len, size_t table_len);
+char *parse_table_name(char *create_query, size_t query_len, size_t table_len);
+
+char *parse_columns_declare(char *create_query, size_t query_len, size_t table_len);
+
 struct create *parse(char *create_query);
 
-int create_data_file(char *table_name);
+int create_table_data_file(char *table_name);
 
-int write_to_fd(int fd, char *query);
+int write_table_structure(int fd, char *query);
 
 /**
- * @param fd           -- File descriptor main database file
- * @param create_query -- Query "<table_name> (field0 type0 [constraints], ..., fieldN typeN [constraints])"
+ * @param fd file descriptor main database file
+ * @param create_query query such as "table_name (column_name0 column_type0 [constraints],
+ *                                      ... ..., column_nameN column_typeN [constraints])"
  */
 int create_table(int fd, char *create_query);
