ebf1f8d43b9036041168bb494d607629c1f8d3f0
ma.zubova@gmail.com <ma.zubova@gmail.com@f5664db2-0e7f-d17f-f7c6-2a055b35f40f>
2011-05-21 09:24:23 +0000

    add func

---
+++ trunk/jabberwocky_func.c
@@ -0,0 +1,41 @@
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include "jabberwocky_func.h"
+
+char * getDBPath(){
+     char *homePath = getenv("HOME");
+    char *dbPath = (char *)malloc(255*sizeof(char));
+    strcpy(dbPath, homePath);
+    strcat(dbPath, "/Jabberwocky/DBs");
+    return dbPath;
+}
+
+int isDBExist(char *base){
+    struct dirent *entry;
+    int ret = 1;
+    char *dbPath = getDBPath(); 
+    DIR *dir = opendir(dbPath);
+    
+    if (!dir){
+		perror("isDBExist");
+		ret = -1;
+	}
+    
+    errno = 0;
+    while((entry = readdir(dir)) != NULL)
+            if(!strcmp(entry->d_name, base)){
+                 ret = 0;
+                 break;
+            }
+    
+    if (errno && !entry){
+		perror("isDBExist");
+		ret = -1;
+	}
+    
+    free(dbPath);
+    closedir(dir);
+    return ret;
+}
