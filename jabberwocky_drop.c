f6e239d1708037f31f38d570b2080e243324a8f2
ma.zubova <ma.zubova@gmail.com>
2011-05-21 09:03:49 +0000

    DONE!!!

--- jabberwocky_drop.c
+++ jabberwocky_drop.c
@@ -10,8 +10,7 @@
 
 #include "jabberwocky_func.h"
 
-int isDBExist(char *);
-char * getDBPath();
+int removeDir(char *);
 
 int main(int argc, char *argv[]){
     if (argc != 2){
@@ -21,8 +20,8 @@ int main(int argc, char *argv[]){
     
     char *dbName = argv[1];
     int exist = isDBExist(dbName);
-    if (exist <= 0){
-       perror("\nDatabase was not found or an error occurred"); 
+    if (exist){
+       printf("\nDatabase was not found or an error occurred.\n"); 
        exit(-1);          
     }
     
@@ -31,11 +30,51 @@ int main(int argc, char *argv[]){
     *buf = '/';
     strcat(buf, dbName);
     strcat(dbPath, buf);
-    if (rmdir(dbPath) < 0){
-       perror("\nError in deleting database");
+    if (removeDir(dbPath)){
+       printf("\nError in deleting database.\n");
        exit(-1);          
     }
     free(buf);
     free(dbPath);
     return 0;
 }
+
+int removeDir(char *dirPath) {
+    strcat(dirPath, "/");
+    DIR *pdir = opendir (dirPath);
+    struct dirent *pent = NULL;
+    if (pdir == NULL) { 
+		perror("removeDir");
+        return -1; 
+    } 
+    char *nextFile = (char *)malloc(255*sizeof(char));
+	errno = 0;
+    int counter = 1; // use this to skip the first TWO which cause an infinite loop (and eventually, stack overflow)
+    while ((pent = readdir (pdir)) != NULL) { 
+        if (counter > 2) {
+            strcpy(nextFile, dirPath);
+            strcat(nextFile, pent->d_name);
+            //struct stat *s;
+            //stat(nextFile, s); 
+            //if (S_ISDIR(s->st_mode)) // TODO: @if@ for directory                 
+                //removeDir(nextFile);
+            //else
+				remove(nextFile);
+        } 
+        else
+			counter++;
+    }
+    
+    if (errno){
+		perror("removeDir");
+		return -1;
+	}
+     
+    closedir (pdir);
+    if (rmdir(dirPath)) {
+		perror("removeDir");
+		return -1; 
+	}
+    return 0;
+}
+
