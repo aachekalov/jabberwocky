657a50e9bba03ef3b5e8eda7666f34746ee2fd51
ma.zubova@gmail.com <ma.zubova@gmail.com@f5664db2-0e7f-d17f-f7c6-2a055b35f40f>
2011-06-04 13:39:47 +0000

    isTableName

--- trunk/insert_into.c
+++ trunk/insert_into.c
@@ -12,5 +12,23 @@ int insert_into(char *query, struct table *tableList){
 	tableName = trim(tableName);
 	char q = query + len +1;
 	q = trim(q);
+   int index = isTableName(tableName, tableList);
+   if (index < 0){
+     printf("Error: non-existent table");
+     return -1;
+   }
+   
 	return 0;
+}
+
+int isTableName(char *name, struct table *tableList){
+   int i;
+   for (i = 0; i < sizeof(tableList)/sizeof(struct table); i++)
+     if (!strcmp(tableList[i], name))
+         return i;
+   return -1;
+}
+
+int checkConstraints (char *q, struct table t){
+
 }
