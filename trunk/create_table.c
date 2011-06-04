807906702e56a5f87974a0ab85225b52086bb530
AAChekalov@gmail.com <AAChekalov@gmail.com@f5664db2-0e7f-d17f-f7c6-2a055b35f40f>
2011-06-04 08:51:22 +0000

    asdf2

--- trunk/create_table.c
+++ trunk/create_table.c
@@ -22,12 +22,14 @@ int check_table_name_length(size_t query_len, size_t table_len) {
 
 char *parse_table_name(char *create_query, size_t query_len, size_t table_len) {
 	if (check_table_name_length(query_len, table_len)) {
+		printf("check_table_name_length error\n");
 		return NULL;
 	}
 	char *table_name = (char *) calloc(table_len + 1, sizeof(char));
 	strncpy(table_name, create_query, table_len);
-	//trim(table_name);
+	table_name = trim(table_name);
 	if (check_identifier_name(table_name)) {
+		printf("'%s' check_identifier_name error\n", table_name);
 		free(table_name);
 		return NULL;
 	}
@@ -36,11 +38,11 @@ char *parse_table_name(char *create_query, size_t query_len, size_t table_len) {
 
 struct column_declare *parse_column_declare(char *column_declare_str) {
 	struct column_declare *column = (struct column_declare *) malloc(sizeof(struct column_declare));
-	trim(column_declare_str);
+	column_declare_str = trim(column_declare_str);
 
-	column->column_name = cutTheFirstWord(column_declare_str, &column_declare_str);
-	//trim(column->column_name);
+	column->column_name = trim(cutTheFirstWord(column_declare_str, &column_declare_str));
 	if (check_identifier_name(column->column_name)) {
+		printf("'%s' check_identifier_name error\n", column->column_name);
 		return NULL;
 	}
 
@@ -54,17 +56,20 @@ struct column_declare *parse_column_declare(char *column_declare_str) {
 	} else if (!strcmp(type, CHAR)) {
 		column->type = 4;
 	} else {
+		printf("type '%s' no allowed\n", type);
 		return NULL;
 	}
 
 	column_declare_str = trim(column_declare_str);
 	char *constraint;
 	while ((constraint = cutTheFirstWord(column_declare_str, &column_declare_str)) != "") {
+		strup(constraint);
 		if (!strcmp(constraint, NOT)) {
 			trim(column_declare_str);
 			if (!strcmp(cutTheFirstWord(column_declare_str, &column_declare_str), _NULL)) {
 				column->constraints += 1;
 			} else {
+				printf("NULL sould be after NOT\n");
 				return NULL;
 			}
 		} else if (!strcmp(constraint, _NULL)) {
@@ -76,6 +81,7 @@ struct column_declare *parse_column_declare(char *column_declare_str) {
 			if (!strcmp(cutTheFirstWord(column_declare_str, &column_declare_str), KEY)) {
 				column->constraints += 4;
 			} else {
+				printf("KEY is missing\n");
 				return NULL;
 			}
 		} else if (!strcmp(constraint, FOREIGN)) {
@@ -84,21 +90,20 @@ struct column_declare *parse_column_declare(char *column_declare_str) {
 				column->constraints += 8;
 				char *foreign_table = trim(cutTheFirstWord(column_declare_str, &column_declare_str));
 			} else {
+				printf("KEY is missing\n");
 				return NULL;
 			}
 		} else {
+			printf("constraint '%s' no allowed\n", constraint);
 			return NULL;
 		}
 		trim(column_declare_str);
 	}
-
-	if (1 == 0)
-		return NULL;
 	return column;
 }
 
 struct column_declare *parse_columns(char *create_query, size_t query_len, size_t table_len) {
-	struct column_declare *columns;
+	struct column_declare *columns = NULL;
 	int column_number = 0;
 	int i;
 	for (i = table_len + 2; i < query_len; i++) {
@@ -120,18 +125,21 @@ struct column_declare *parse_columns(char *create_query, size_t query_len, size_
 
 struct table *parse(char *create_query) {
 	struct table *result = (struct table *) malloc(sizeof(struct table));
-	//trim(create_query);
+	create_query = trim(create_query);
 	size_t query_len = strlen(create_query);
 	size_t table_len = strcspn(create_query, "(");
+	//printf("\n\n%s#%d#%d\n\n", create_query, query_len, table_len);
 
 	result->table_name = parse_table_name(create_query, query_len, table_len);
 	if (result->table_name == NULL) {
+		printf("parse_table_name error\n");
 		free(result);
 		return NULL;
 	}
 
 	result->columns = parse_columns(create_query, query_len, table_len);
 	if (result->columns == NULL) {
+		printf("parse_columns error\n");
 		free(result);
 		return NULL;
 	}
@@ -149,10 +157,12 @@ int write_table_structure(int fd, struct table *new_table) {
 	return 0;
 }
 
-int create_table(int fd, char *create_query, struct table * listTable) {
+int create_table(int fd, char *create_query, struct table *table_list) {
 	struct table *result = parse(create_query);
-	if (result == NULL)
+	if (result == NULL) {
+		printf("parse error\n");
 		return -1;
+	}
 	create_table_data_file(result->table_name);
 	write_table_structure(fd, result);
 	return 0;
