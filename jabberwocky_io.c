7770311080f87051fc068058a46265ded252032f
ma.zubova <ma.zubova@gmail.com>
2011-06-04 06:54:02 +0000

    io

--- jabberwocky_io.c
+++ jabberwocky_io.c
@@ -57,8 +57,62 @@ int write (int fd, struct table newTable){
 }
 
 int getFieldIndex(int fd, char* tableName, char* fieldName){
-	
-	
+	struct stat st;
+	if(fstat(fd,&st) < 0)    
+        return 1;
+    if (st.st_size == 0)
+		return NULL;
+	int k = 1, i;
+	struct table * tableList;	
+	int index = -1;
+	while(!EOF){
+		tableList = (struct table *)malloc(sizeof(struct table));
+		int len;
+		if(read(fd, &len, sizeof(int)) != sizeof(int)){
+			perror("Reading error");
+			return -1;
+		}
+		tableList->table_name = (char *)malloc(len * sizeof(char));
+		if(read(fd, tableList->table_name, len)) != strlen(tableList->table_name)){
+			perror("Reading error");
+			return -1;
+		}
+		if (!strcmp(tableName, tableList->table_name)){
+			unsigned char qcol;
+			if(read(fd, &tableList->column_count, sizeof(unsigned char)) != sizeof(unsigned char)){
+				perror("Reading error");
+				return -1;
+			}
+			
+			tableList->columns = (struct column_declare *)calloc(qcol, sizeof(struct column_declare));			
+			for(i = 0; i < qcol; i++){
+				int nextIndex;
+				if(read(fd, &nextIndex, sizeof(int)) != sizeof(int)){
+					perror("Reading error");
+					return -1;
+				}
+				int clen;
+				if(read(fd, &clen, sizeof(int)) != sizeof(int)){
+					perror("Reading error");
+					return -1;
+				}
+				tableList->columns[i].column_name = (char *)malloc(clen * sizeof(char));
+				if(read(fd, tableList.columns[i].column_name, clen) != clen){
+					perror("Reading error");
+					return -1;
+				}
+				if (!strcmp(columns[i].column_name, fieldName)){
+					index = nextIndex;
+					break;
+				}
+			}
+			break;
+		}else{
+			continue;
+		}
+	}
+		
+	return index;	
 }
 
 int read (int fd, struct table *tableList){
@@ -68,7 +122,8 @@ int read (int fd, struct table *tableList){
         return 1;
     if (st.st_size == 0)
 		return NULL;
-	int k = 1, i;	
+	int k = 1, i;
+	struct table * tableList;	
 	while(!EOF){
 		tableList = (struct table *)realloc(tableList, k * sizeof(struct table));
 		int len;
@@ -77,7 +132,7 @@ int read (int fd, struct table *tableList){
 			return -1;
 		}
 		tableList[k-1].table_name = (char *)malloc(len * sizeof(char));
-		if(read(fd, tableList[k-1].table_name, strlen(tableList[k-1].table_name)) != strlen(tableList[k-1].table_name)){
+		if(read(fd, tableList[k-1].table_name, len)) != strlen(tableList[k-1].table_name)){
 			perror("Reading error");
 			return -1;
 		}
@@ -86,8 +141,9 @@ int read (int fd, struct table *tableList){
 			perror("Reading error");
 			return -1;
 		}
+		tableList[k-1].columns = (struct column_declare *)calloc(qcol, sizeof(struct column_declare));
+		k++;	
 		for(i = 0; i < qcol; i++){
-			tableList.columns = (struct column_declare *)realloc(tableList.columns, qcol * sizeof(struct column_declare));	
 			if(read(fd, &lastIndex, sizeof(int)) != sizeof(int)){
 				perror("Reading error");
 				return -1;
@@ -97,8 +153,16 @@ int read (int fd, struct table *tableList){
 				perror("Reading error");
 				return -1;
 			}
-			char* tableName = (char *)malloc(len * sizeof(char));
-			if(read(fd, tableName, strlen(tableName)) != strlen(tableName)){
+			tableList[k-1].columns[i].column_name = (char *)malloc(clen * sizeof(char));
+			if(read(fd, tableList[k-1].columns[i].column_name, clen) != clen){
+				perror("Reading error");
+				return -1;
+			}
+			if(read(fd, tableList[k-1].columns[i].type, sizeof(char)) != sizeof(char)){
+				perror("Reading error");
+				return -1;
+			}
+			if(read(fd, tableList[k-1].columns[i].constraints, sizeof(char)) != sizeof(char)){
 				perror("Reading error");
 				return -1;
 			}
