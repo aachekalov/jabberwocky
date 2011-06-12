3a7c8637ac624338f54a5d1fce8c32554f7c8871
ma.zubova <ma.zubova@gmail.com>
2011-06-12 15:35:56 +0000

    вп

--- jabberwocky_select.c
+++ jabberwocky_select.c
@@ -13,7 +13,7 @@
 #include "jabberwocky_io.h"
 #include "insert_into.h"
 
-int getDBFD(char *);
+int getDBFD(char *str, int flags);
 char * getDBDirStr(char *base);
 
 
@@ -24,45 +24,50 @@ int main(int argc, char *argv[]){
     }
     
     char *dbName = argv[1];
-    int fd = getDBFD(dbName);
+    int fd = getDBFD(dbName, O_RDONLY);
     if (fd < 0){
        perror("\nError in db opening"); 
        exit(-1);         
     }
     char *dbPath = getDBDirStr(dbName);
+
     struct table *tableList = NULL;
-    int q = readTables(fd, &tableList);    
-    printf("\nq = %d\n", q);
-    while(1){
+    int q = readTables(fd, &tableList); 
+
+	int dirtyBit = 0;   
+    while(1){        
        printf("%s > ", dbName);
        char *query, *firstWord;
-       query = (char *)malloc(1024); //!!!
+       query = (char *)calloc(1024, sizeof(char)); //!!!
        fgets(query, 1024, stdin);
        char *newquery = trim(query);
        char *operation = cutTheFirstWord(newquery, &newquery);
        strup(operation); // Special for Alexey 
        newquery = trim(newquery);
+
        
        int ret = 0;
        if (!strcmp(operation, "CREATE")){
              firstWord = cutTheFirstWord(newquery, &newquery);
-             //TODO: firstWord == TABLE
-             ret = create_table(fd, dbPath, trim(newquery), &tableList, q);
-             if (ret == 0)
-                q++;
-             else printf("CREATE TABLE: FAIL\n");
+             ret = create_table(fd, dbPath, trim(newquery), &tableList, q); 
+             if (ret == 0){
+				dirtyBit = 1;
+                q++; 
+	     	 }
        }else if (!strcmp(operation, "INSERT")){ 
              firstWord = cutTheFirstWord(newquery, &newquery);
              printf("\ninsertinto works with \"%s\"\n", newquery);
-             ret = insert_into(trim(newquery), tableList, dbPath);  
+             ret = insert_into(trim(newquery), tableList, q, dbPath);  
        }else if (!strcmp(operation, "UPDATE")){
              printf("\nupdate works with \"%s\"\n", newquery);//ret = update(fd, newquery); 
        }else if (!strcmp(operation, "DROP")){
              firstWord = cutTheFirstWord(newquery, &newquery);
              printf("\ndroptable works with \"%s\"\n", newquery);
-             ret = drop_table(fd, dbPath, trim(newquery), tableList, q);
-             if (ret == 0)
+             ret = drop_table(fd, dbPath, trim(newquery), &tableList, q);
+             if (ret == 0){
+				dirtyBit = 1;
                 q--; 
+	      	 }
        }else if (!strcmp(operation, "DELETE")){
              firstWord = cutTheFirstWord(newquery, &newquery);
              printf("\ndeletefrom works with \"%s\"\n", newquery);//ret = delete_from(fd, trim(newquery));      
@@ -78,33 +83,43 @@ int main(int argc, char *argv[]){
              ret = -1;   // here must be a timer, pthread_create and some cruel line   
        }
        if (ret < 0)
-             printf("\tWrong query. Please, be more attentive.\n");
-       
+             printf("\n\tWrong query. Please, be more attentive.\n");
+        
        free(query);
-       
     }
+	
     
-    lseek(fd, 0, SEEK_SET);
-    printf("\n1q = %d\n", q);
-    writeTables(fd, tableList, q);
-    printf("\n2q = %d\n", q);
-    free(tableList);
-    printf("\n3q = %d\n", q);
+    if(dirtyBit){
+	    close(fd);
+		fd = getDBFD(dbName, O_WRONLY|O_TRUNC);
+		if (fd < 0){
+       		perror("\nError in db file opening"); 
+       		exit(-1);         
+    	}
+		lseek(fd, 0, SEEK_SET);
+	    writeTables(fd, tableList, q);
+    }
+	int i, j;
+	for (i = 0; i < q; i++){
+		for (j = 0; j < tableList[i].column_count; j++)
+			free(tableList[i].columns[j].column_name);
+		free(tableList[i].columns);
+		free(tableList[i].table_name);		
+	} 
+	free(tableList);
     free(dbPath);
-    printf("\n4q = %d\n", q);
     close(fd);
     return 0;   
 }
 
-int getDBFD(char *base){
+int getDBFD(char *base, int flags){
     char *dbPath = getDBPath();
     
     strcat(dbPath, "/");
     strcat(dbPath, base);
     strcat(dbPath, "/.");
     strcat(dbPath, base); 
-    int fd = open(dbPath, O_RDWR);// | O_APPEND);
-    
+    int fd = open(dbPath, flags);
     free(dbPath);
     
     return fd;
