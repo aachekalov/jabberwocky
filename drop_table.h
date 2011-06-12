20d45a23dd1ec5532f594de66b170344ad1a41fe
ma.zubova <ma.zubova@gmail.com>
2011-06-12 15:25:13 +0000

    не знаю даже, что изменилось

--- drop_table.h
+++ drop_table.h
@@ -1,6 +1,6 @@
 //#include "jabberwocky_io.h"
 
-int drop_table(int fd, char *dbPath, char *query, struct table *tableList, int size);
+int drop_table(int fd, char *dbPath, char *query, struct table **tableList, int size);
 
 int removeTable(char *tableName, struct table **tableList, int size);
 
